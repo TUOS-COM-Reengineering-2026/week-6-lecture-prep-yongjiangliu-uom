71dabfe657363558b3adc13b91a5b871d43de40e
yongjiang liu <yongjiangliu129@gmail.com>
2026-03-17 14:22:11 +0000

    Add branch coverage tests for strange_function

--- tests/test_prep.py
+++ tests/test_prep.py
@@ -8,4 +8,32 @@ class MyTestCase(unittest.TestCase):
             second='behaviour 3'
         )
 
-    # TODO: Can you write more test cases below to increase the test coverage of `strange_function`?
+    def test_strange_function_behaviour_1(self):
+        self.assertEqual(
+            first=strange_function(2, 2, 1, 3),
+            second='behaviour 1'
+        )
+
+    def test_strange_function_behaviour_2(self):
+        self.assertEqual(
+            first=strange_function(2, 2, 5, 3),
+            second='behaviour 2'
+        )
+
+    def test_strange_function_behaviour_4(self):
+        self.assertEqual(
+            first=strange_function(5, 3, 4, 1),
+            second='behaviour 4'
+        )
+
+    def test_strange_function_behaviour_5(self):
+        self.assertEqual(
+            first=strange_function(5, 3, 4, 6),
+            second='behaviour 5'
+        )
+
+    def test_strange_function_behaviour_6(self):
+        self.assertEqual(
+            first=strange_function(5, 3, 5, 6),
+            second='behaviour 6'
+        )
